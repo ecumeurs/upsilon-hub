34677b3fbc8118014a6e7ffaa2f5ba919c36f8bc
bastien barcaioni <bastien.barcaioni@gmail.com>
2026-04-16 07:40:39 +0000

    chore: formalize submodules and split upsiloncli/upsilonapi into separate repositories

--- upsiloncli/samples/sender.js
+++
@@ -1,4 +0,0 @@
-// sender.js
-upsilon.log("Sender: Setting shared match_id...");
-upsilon.setShared("match_id", "ABC-123");
-upsilon.log("Sender: Done.");
--- upsiloncli/samples/test_teardown_crash.js
+++
@@ -1,9 +0,0 @@
-// test_teardown_crash.js
-
-upsilon.onTeardown(() => {
-    upsilon.log("TEARDOWN: This MUST run even on crash!");
-});
-
-upsilon.log("Forcing a crash with unhandled assertion failure...");
-upsilon.assert(false, "UNHANDLED CRASH");
-upsilon.log("This line should NEVER be reached.");
